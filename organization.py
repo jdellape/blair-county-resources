46f7ac5fe9c802a8b37f9c31f869f0090f0462ac
John Dellape <dellapejw@gmail.com>
2023-06-26 21:07:58 -0400

    add, edit and view version one

--- organization.py
+++ organization.py
@@ -1,14 +1,13 @@
 #File for defining the organization object
 class Organization:
-    def __init__(self, name, address_line_one, city, zip_code, phone_num, hours_of_operation):
+    def __init__(self, name, address_line_one, city, zip_code, phone_num):
         self.name = name
         self.address_line_one = address_line_one
         self.city = city
         self.state_code = 'PA'
         self.zip_code = zip_code
         self.phone_num = phone_num
-        self.hours_of_operation = hours_of_operation.__dict__
-        self.services = []
+        self.services = {} #initialize as empty dictionary
 
     #Add additional setters for attributes that exist for some but not all orgs
     def set_contact(self, value):
@@ -24,6 +23,9 @@ class Organization:
     #Service objects will contain weekly schedule objects (if applicable)
     def add_service(self, value):
         #TODO: value should be array. Can add validation here
-        self.services.append(value.__dict__)
+        if value.has_schedule == False:
+            self.services[value.name] = {'has_schedule':value.has_schedule}
+        elif value.has_schedule == True:
+            self.services[value.name] = {'has_schedule':value.has_schedule, 'schedule':value.schedule}
 
     
